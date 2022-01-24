552d499a0ab9713818b685a44e3e073e634cb207
Kacper <klimaskacper@interia.pl>
2022-01-24 23:33:47 +0100

    buttons and divs

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 
 
-function showMessage(){
-    document.getElementById("message").innerHTML = "JOB DONE";
+function showMessage(){    
+    if (document.getElementById("message").innerHTML == "JOB DONE"){
+        document.getElementsByClassName("button").innerHTML = "Hide"
+        document.getElementById("message").innerHTML = "UPS sth gone wrong"    
+    } else {
+        document.getElementsByClassName("button").innerHTML = "Show"
+        document.getElementById("message").innerHTML = "JOB DONE";
+    }
+    
+    
+
 }
