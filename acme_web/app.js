99ffbb4673b619d62d0416895d3f9d467aaa8156
Kacper <klimaskacper@interia.pl>
2022-01-28 18:37:30 +0100

    js

--- acme_web/app.js
+++ acme_web/app.js
@@ -1,13 +1,19 @@
 
 
 function check(){
-    
-    
-
     let form = document.getElementById("form");
     
     let login = form.elements["login"].value;
     let password = form.elements["password"].value;
 
-    if (login != '') alert("login is empty")
+    let h1 = document.getElementById("message")
+
+    if (login == '' || password == ''){
+        h1.innerHTML = "login or password is empty";
+    }
+    else {
+        h1.innerHTML = "checking";
+    }
+
+
 }
