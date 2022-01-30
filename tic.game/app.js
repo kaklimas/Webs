ae67b7b8cce0e5355826f36d1b3bf589dca8ddbb
Kacper <klimaskacper@interia.pl>
2022-01-30 15:32:40 +0100

    tic

--- tic.game/app.js
+++ tic.game/app.js
@@ -1,3 +1,14 @@
+let currentPlayer = "X";
+
+
 function draw(id){
-    document.getElementById(id).innerHTML = "X";
+    document.getElementById(id).innerHTML = currentPlayer;
+    let info = document.getElementById("info");
+    
+    if (currentPlayer == "X") currentPlayer = "Y"
+    else currentPlayer = "X"
+    info.innerHTML = "Current move: " + currentPlayer;
+    
+    
+    
 }
