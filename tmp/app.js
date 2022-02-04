1f2a3b3a4a723c16c44d2d688123831252a9c21a
Kacper <klimaskacper@interia.pl>
2022-02-04 08:05:17 +0100

    addToBox

--- tmp/app.js
+++ tmp/app.js
@@ -2,3 +2,7 @@
 function change(color){
     document.getElementById('img').style.background = color;
 }
+function add(){
+    let prop = "Item was added to box, properties: color: " + document.getElementById('img').style.background + ", price: 123$";
+    alert(prop);
+}
